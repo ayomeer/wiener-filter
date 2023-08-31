9d0a46cca185067f14b7c0599bf03af6bc41c088
DESKTOP-0SRFR83\ayome <andreas.kuhn@bluewin.ch>
2023-08-31 22:01:14 +0200

    implemented wienerK, working on wienerC (local variance getter)

--- wiener-filter.py
+++ wiener-filter.py
@@ -2,19 +2,22 @@ import numpy as np
 from scipy.signal import convolve2d
 # from convolution2D import convolve2D
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib.widgets import Slider
 
-# --- Config ------------------------------------------------
+# --- Config ------------------------------------------------------------
 H_TYPE = 'box' # degradation function
-NOISE_INTENSITY = 0.01
+FILTER_METHOD = 'wienerK' # options: 'inverse', 'wienerK', 'wienerC'  
 
+NOISE_STD_DEV = 0.01
+WIENER_K = NOISE_STD_DEV * 2
 
-# --- Setup -------------------------------------------------
-# -- Test image 
+# --- Setup ---------------------------------------------------------
+# Generate test image
 M = N = 100
 testImg = np.zeros((M, N))
 
-# Add white square in the center
-a = 10 # square side length 
+a = 10 # white square side length 
 whiteIdxRange_m = np.arange((M-a)/2, (M+a)/2).astype(int)
 whiteIdxRange_n = np.arange((N-a)/2, (N+a)/2).astype(int)
 
@@ -23,7 +26,7 @@ for m in whiteIdxRange_m:
         testImg[m, n] = 1
 
 
-# -- point spread functions (kernels) 
+# Generate point spread functions (kernels) 
 # Box Kernel
 kernelSize = 5
 h_box = (1/kernelSize**2)*np.ones((kernelSize, kernelSize))
@@ -32,35 +35,89 @@ h_box = (1/kernelSize**2)*np.ones((kernelSize, kernelSize))
 # TODO: Implement Gaussian Blur Kernel
 
 
-# -- Zero Mean (Gaussian) White Noise
-n = np.random.normal(0, NOISE_INTENSITY, (M,N))
+# Generate noise degradation
+n = np.random.normal(0, NOISE_STD_DEV, (M,N))
 
-# -- Degrade Image 
+# Apply degradations to test image
 g = convolve2d(testImg, h_box, mode='same') + n
 
-plt.figure(); plt.title('blurred image')
-plt.imshow(g, cmap='gray')
-plt.show(block=False)
+# --- Helper Functions --------------------------------------------------
+def getMinNeighborhoodVariance(g):
+    pass # TODO implement local variances
+
+# --- Image Restoration Functions ---------------------------------------
+def inverseFiltering(g, h): 
+    # -- Transform image and degradation functions into fourier domain
+    P = (M + h.shape[0]-1, N + h.shape[1]-1) # avoid circular conv
+    
+    G = np.fft.fft2(g, P)
+    H = np.fft.fft2(h, P)
+
+    F_hat = G/H
+    f_hat = np.fft.ifft2(F_hat, P)
+
+    return f_hat
+
+def wienerFiltering(g, h, K):
+    P = (M + h.shape[0]-1, N + h.shape[1]-1) # avoid circular conv
+    
+    G = np.fft.fft2(g, P)
+    H = np.fft.fft2(h, P)
+
+    W = (1/H) * (np.abs(H)**2)/(np.abs(H)**2 + K)
+   
+    F_hat = G*W
+    f_hat = np.fft.ifft2(F_hat, P)
+    
+    return np.real(f_hat)
+
+if __name__ == '__main__':
+
+    # Show degraded image to be restored
+    plt.figure(); plt.title('blurred image')
+    plt.imshow(g, cmap='gray')
+    plt.show(block=False)
 
 
-# --- Image Restoration ---------------------------------------- 
-if H_TYPE == 'box': 
-    h = h_box
-if H_TYPE == 'gaussian':
-    pass # TODO
+    # --- Image Restoration -------------------------------------------
+    if H_TYPE == 'box': 
+        h = h_box
+    if H_TYPE == 'gaussian':
+        pass # TODO 
+    
 
-# Transform image and degradation functions into fourier domain
+    match FILTER_METHOD:
+        case 'inverse':
+            f_hat = inverseFiltering(g, h)
 
-# Determine padding P=L+M to avoid circular convolution problem
-P = (M + h.shape[0]-1, N + h.shape[1]-1)
- 
-G = np.fft.fft2(g, P)
-H = np.fft.fft2(h, P)
+            plt.figure(); plt.title('unblurred image')
+            plt.imshow(abs(f_hat), cmap='gray')
+            plt.show()
+    
+        case 'wienerK':
+            # Set up figure
+            gs = gridspec.GridSpec(10,4)
+            fig = plt.figure()
+            axImg =    fig.add_subplot(gs[0:8,:])
+            
+            # Set up slider to change K interactively
+            axSlider = fig.add_subplot(gs[9,:])
+            sl = Slider(axSlider, 'K', 0, 0.2, valinit=0, valstep=0.001)
 
-F_hat = G/H
-f_hat = np.fft.ifft2(F_hat, P)
+            def sl_changed(sliderK):
+                f_hat = wienerFiltering(g, h, sliderK)
+                im.set_data(f_hat)
 
-plt.figure(); plt.title('unblurred image')
-plt.imshow(abs(f_hat), cmap='gray')
-plt.show()
+            sl.on_changed(sl_changed)
+
+            # Do filtering and show result
+            f_hat = wienerFiltering(g, h, WIENER_K)
+            im = axImg.imshow(f_hat, cmap='gray')
+            
+        case 'wienerC':
+            pass
+
+    plt.show(block=True)
+
+
 
