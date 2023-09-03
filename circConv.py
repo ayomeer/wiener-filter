02e66166a6a435b330992e221e58878624a80670
DESKTOP-0SRFR83\ayome <andreas.kuhn@bluewin.ch>
2023-09-03 18:55:19 +0200

    ready for presentation

--- circConv.py
+++ circConv.py
@@ -5,22 +5,25 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import Slider
 
 # --- Config ------------------------------------------------------------------------
-
+CIRCULAR = True
 
 
 # --- Setup -------------------------------------------------------------------------
+L = 400 # number of samples of signals to convolve
+N = 400 # initial number of samples in fourier domain
+P = 2*L-1
 
-N = 400
-n = np.arange(N)
+# Functions to convolve (initial state)
+n = np.arange(L) 
+f = np.zeros_like(n).astype(float); f[1:300] = 1.0; # f[-50:-1] = 0.5
+h = np.zeros_like(n).astype(float); h[1:200] = 0.5; # h[-50:-1] = 0.5
+h_ = np.flip(h)
 
-# Functions to convolve
-f = np.zeros_like(n).astype(float); f[1:300] = 1.0
-h = np.zeros_like(n).astype(float); h[1:200] = 0.5
+# circular conv in fourier domain
+g = np.real(np.fft.ifft(np.fft.fft(f)*np.fft.fft(h)))
 
-g = np.fft.ifft(np.fft.fft(f)*np.fft.fft(h)) # circular conv in fourier domain
-
-def plot():
-        # Set up convolution visualization w/ slider
+def setupCircConvPlot():
+    # Set up convolution plot
     gs = gridspec.GridSpec(12,6)
     fig = plt.figure()
     ax_inputs = fig.add_subplot(gs[0:4,:])
@@ -28,27 +31,36 @@ def plot():
     ax_xSlider = fig.add_subplot(gs[10,:])
     ax_NSlider = fig.add_subplot(gs[11,:])
    
-    ax_inputs.vlines([0, 400], 0, 1.2, 'black')
-    ax_inputs.plot(n, f)
-    l_h,  = ax_inputs.plot(-n, h, 'b-')
-    # l_hl, = ax_inputs.plot(-n-n.size, h, 'b--')
-    l_hr, = ax_inputs.plot(-n+n.size, h, 'b--')
+    # visual config
+    ax_inputs.set_xlim(-800, 600)
+    ax_output.set_xlim(-20, 820)
 
-    h_ = np.flip(h)
-    # c_fill = ax_inputs.fill_between(n, np.minimum(h_, f))
+    # Plot actual functions
+    l_f, = ax_inputs.plot(n, f)
+    l_h, = ax_inputs.plot(-n, h, color='C2')
+    l_g, = ax_output.plot(n, g, color='C1', zorder=1)
+    p_g = ax_output.scatter(0, g[0], color='orange', zorder=2)
 
-    l_g, = ax_output.plot(n, g, color='C1'); ax_output.set_ylim(ymin=0)
-    
+    ax_output.set_ylim(ymin=-20, ymax=120)
 
-    sl_x = Slider(ax_xSlider, 'x', 0, N, valinit=0, valstep=1)
-    sl_N = Slider(ax_NSlider, 'N', 400, n.size*2, valinit=400, valstep=1)
+    if CIRCULAR == True:
+        l_hr, = ax_inputs.plot(-n+n.size, h, color='C2', linestyle='--', alpha=0.65)
+        l_hl, = ax_inputs.plot(-n-n.size, h, color='C2', linestyle='--', alpha=0.65)
+        
+        l_gr, = ax_output.plot(n+N, g, color='C1', linestyle='--')
 
-    
+
+    sl_x = Slider(ax_xSlider, 'x', 0, P, valinit=0, valstep=1)   
     def sl_x_changed(x):
+        _, f = l_f.get_data()
+        _, h = l_h.get_data()
+
         # update inputs plot
         l_h.set_xdata(-n+x)
-        # l_hl.set_xdata(-n-n.size+x)
-        l_hr.set_xdata(-n+n.size+x)
+
+        if CIRCULAR == True:
+            l_hl.set_xdata(-n-n.size+x)
+            l_hr.set_xdata(-n+n.size+x)
 
         # update shading
         h_ = np.roll(np.flip(h), x)
@@ -56,20 +68,58 @@ def plot():
         sl_x_changed.c_fill = ax_inputs.fill_between(n, np.minimum(h_, f), alpha=0.4, color='C1')
 
         # TODO: update indicator on conv output plot
+        p_g.set_offsets([x, np.roll(g, -x)[0]]) # roll for repeated spectra
 
-        fig.canvas.draw()
-        fig.canvas.flush_events()
     sl_x_changed.c_fill = ax_inputs.fill_between(n, np.minimum(h_, f), alpha=0.4, color='C1')
-    
-    def sl_N_changed(N):
-        # TODO: change padding on f,g -> update inputs plot
-        # TODO: recompute convolution in fourier domain -> update output plot
-        pass
-
     sl_x.on_changed(sl_x_changed)
+
+    sl_N = Slider(ax_NSlider, 'N', N, P, valinit=f.size, valstep=1)
+    def sl_N_changed(N):
+        global f, h, g, n
+
+        # Update vertical lines indicating nyquist interval
+        sl_N_changed.v_g.remove()
+        sl_N_changed.v_g = ax_output.vlines([0, N], 0, 120, 'black') # New Nyquist Interval
+
+        # update vlines indicating FFT convolution summation range
+        sl_N_changed.v_in.remove()
+        sl_N_changed.v_in = ax_inputs.vlines([0, N], 0, 1.2, 'black')
+
+        # Change padding on f,g
+        n = np.arange(N)
+        f_pad = np.pad(f, (0, N-f.size))
+        h_pad = np.pad(h, (0, N-h.size))
+
+        # Update inputs plot
+        l_f.set_data(n, f_pad)
+        l_h.set_data(-n, h_pad)
+        
+        if CIRCULAR == True:
+            l_hr.set_data(-n+n.size, h_pad)
+            l_hl.set_data(-n-n.size, h_pad)
+
+        # update fill
+        sl_x_changed(0)
+
+        # recompute convolution in fourier domain -> update output plot
+        g = np.real(np.fft.ifft(np.fft.fft(f_pad)*np.fft.fft(h_pad)))
+        l_g.set_data(n, g)
+
+        if CIRCULAR == True:
+            l_gr.set_data(n+N, g)
+
+    sl_N_changed.v_g = ax_output.vlines([0, N], 0, 120, 'black') # Nyquist Interval
+    sl_N_changed.v_in = ax_inputs.vlines([0, N], 0, 1.2, 'black')
     sl_N.on_changed(sl_N_changed)
+
 
     plt.show()
 
 if __name__ == '__main__':
-    plot()
+    # Compute convolution in time domain for reference
+    g_lin = np.convolve(f,h)
+    plt.figure(); plt.title('linear convolution in time domain')
+    plt.plot(np.arange(g_lin.size), g_lin, color='C1')
+    plt.show(block=False)
+
+    setupCircConvPlot()
--- wiener-filter.py
+++ wiener-filter.py
@@ -5,25 +5,37 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.widgets import Slider
 
-# --- Config ------------------------------------------------------------
-H_TYPE = 'box' # options: 'box', 'gaussiain', ('sinc')
-FILTER_METHOD = 'wienerC' # options: 'inverse', 'wienerK', 'wienerC'  
 
-NOISE_STD_DEV = 0.01
+# --- Config ------------------------------------------------------------
+INPUT_IMG = 'generated' # options: 'generated', 'aerial'
+
+H_TYPE = 'box' # options: 'box', 'gaussian', ('sinc')
+FILTER_METHOD = 'inverse' # options: 'inverse', 'wienerK', 'wienerC'  
+
+NOISE_ENABLE = True # options: True, False
+NOISE_STD_DEV = 0.01 
 WIENER_K = NOISE_STD_DEV * 2
 
 # --- Setup ---------------------------------------------------------
 # Generate test image
-M = N = 100
-testImg = np.zeros((M, N))
+match INPUT_IMG:
+    case 'generated':
+        # generate white square test image
+        M = N = 100
+        testImg = np.zeros((M, N))
 
-a = 10 # white square side length 
-whiteIdxRange_m = np.arange((M-a)/2, (M+a)/2).astype(int)
-whiteIdxRange_n = np.arange((N-a)/2, (N+a)/2).astype(int)
+        # add white square
+        a = 10 
+        testImg[M//2-a//2:M//2+a//2, N//2-a//2:N//2+a//2] = 1
 
-for m in whiteIdxRange_m:
-    for n in whiteIdxRange_n:
-        testImg[m, n] = 1
+        # add white line at edge (testing for boundary errors)
+        testImg[M//2:-1, -3:-1] = 1
+
+        f = testImg
+    
+    case 'aerial':
+        f = plt.imread('img/aerial_gs.png')
+        M, N = f.shape
 
 
 # Generate point spread functions (kernels) 
@@ -34,32 +46,37 @@ h_box = (1/kernelSize**2)*np.ones((kernelSize, kernelSize))
 # Gaussian Blur
 # TODO: Implement Gaussian Blur Kernel
 
+if H_TYPE == 'box': 
+    h = h_box
+if H_TYPE == 'gaussian':
+    pass # TODO 
+
 
 # Generate noise degradation
-n = np.random.normal(0, NOISE_STD_DEV, (M,N))
+match NOISE_ENABLE:
+    case True:
+        n = np.random.normal(0, NOISE_STD_DEV, (M+h.shape[0]-1, N+h.shape[1]-1))
+    case False:
+        n = 0
 
 # Apply degradations to test image
-g = convolve2d(testImg, h_box, mode='same') + n
-
-# --- Helper Functions --------------------------------------------------
-def getMinNeighborhoodVariance(g):
-    pass # TODO implement local variances
+g = convolve2d(f, h, mode='full') + n
 
 # --- Image Restoration Functions ---------------------------------------
 def inverseFiltering(g, h): 
     # -- Transform image and degradation functions into fourier domain
-    P = (M + h.shape[0]-1, N + h.shape[1]-1) # avoid circular conv
+    P = (g.shape[0] + h.shape[0]-1, g.shape[1] + h.shape[1]-1) # avoid circular conv
     
     G = np.fft.fft2(g, P) # input zero-padded before transform
     H = np.fft.fft2(h, P) 
 
     F_hat = G/H
-    f_hat = np.fft.ifft2(F_hat, P)
+    f_hat = np.fft.ifft2(F_hat)
 
-    return f_hat
+    return np.real(f_hat)
 
 def wienerKFiltering(g, h, K):
-    P = (M + h.shape[0]-1, N + h.shape[1]-1) # avoid circular conv
+    P = (g.shape[0] + h.shape[0]-1, g.shape[1] + h.shape[1]-1) # avoid circular conv
     
     G = np.fft.fft2(g, P)
     H = np.fft.fft2(h, P)
@@ -67,19 +84,19 @@ def wienerKFiltering(g, h, K):
     W = (1/H) * (np.abs(H)**2)/(np.abs(H)**2 + K)
    
     F_hat = G*W
-    f_hat = np.fft.ifft2(F_hat, P)
+    f_hat = np.fft.ifft2(F_hat)
     
     return np.real(f_hat)
 
 
-NOISE_POWER = (M*N)**2*NOISE_STD_DEV**2
+NOISE_POWER = (M*N)**2*NOISE_STD_DEV**2 # TODO: get this from local neighborhood variance
 def wienerCFiltering(g, h, gamma):
-    P = (M + h.shape[0]-1, N + h.shape[1]-1) # avoid circular conv
+    P = (g.shape[0] + h.shape[0]-1, g.shape[1] + h.shape[1]-1) # avoid circular conv
 
     G = np.fft.fft2(g, P)
     H = np.fft.fft2(h, P)
 
-    u, v = np.arange(P[0]), np.arange(P[1])
+    u, v = np.arange(P[1]), np.arange(P[0]) # shape interpreted differntly(!)
     uu, vv = np.meshgrid(u, v)
     C = -4*np.pi**2*(uu**2+vv**2)
 
@@ -88,39 +105,38 @@ def wienerCFiltering(g, h, gamma):
     F_hat = G*W
 
     phi = sum(sum(abs(G-H*F_hat)**2))
-
-    f_hat = np.fft.ifft2(F_hat, P)
+    f_hat = np.fft.ifft2(F_hat) 
     
     return np.real(f_hat), phi
 
+# --- Helper Functions --------------------------------------------------
+def getMinNeighborhoodVariance(g):
+    pass # TODO implement local variances
 
+
+# --- MAIN -------------------------------------------------------------------------------------
 if __name__ == '__main__':
-
-    # Show degraded image to be restored
-    plt.figure(); plt.title('blurred image')
-    plt.imshow(g, cmap='gray')
+    # Show original- and distorted image
+    fig0 = plt.figure(figsize=(7,9))
+    im0 = fig0.add_subplot(211); im0.set_title('original image')
+    im1 = fig0.add_subplot(212); im1.set_title('degraded (blurred + noise)')
+    im0.imshow(f, cmap='gray')
+    im1.imshow(g, cmap='gray')
     plt.show(block=False)
 
-
     # --- Image Restoration -------------------------------------------
-    if H_TYPE == 'box': 
-        h = h_box
-    if H_TYPE == 'gaussian':
-        pass # TODO 
-    
-
     match FILTER_METHOD:
         case 'inverse':
             f_hat = inverseFiltering(g, h)
 
             plt.figure(); plt.title('unblurred image')
-            plt.imshow(abs(f_hat), cmap='gray')
+            plt.imshow(f_hat[0:M, 0:N], cmap='gray')
             plt.show()
     
         case 'wienerK':
             # Set up figure
             gs = gridspec.GridSpec(10,4)
-            fig = plt.figure()
+            fig = plt.figure(figsize=(6,7))
             axImg = fig.add_subplot(gs[0:8,:])
             
             # Set up slider to change K interactively
@@ -129,36 +145,41 @@ if __name__ == '__main__':
 
             def sl_changed(sliderK):
                 f_hat = wienerKFiltering(g, h, sliderK)
-                im.set_data(f_hat)
+                im.set_data(f_hat[0:M, 0:N])
 
             sl.on_changed(sl_changed)
 
             # Do filtering and show result
             f_hat = wienerKFiltering(g, h, WIENER_K)
-            im = axImg.imshow(f_hat, cmap='gray')
+            im = axImg.imshow(f_hat[0:M, 0:N], cmap='gray')
+            
             
         case 'wienerC':
             # Set up figure
             gs = gridspec.GridSpec(10,4)
-            fig = plt.figure()
+            fig = plt.figure(figsize=(6,7))
             axImg = fig.add_subplot(gs[0:8,:])
-            fig.text(0.6, 0.95, "noise power = {}".format(NOISE_POWER))
+            fig.text(0.6, 0.95, "noise power = {:e}".format(NOISE_POWER))
             t = fig.text(0.1, 0.95 ," ")
 
-            # Set up slider to change K interactively
+            # Set up slider to change gamma interactively
             axSlider = fig.add_subplot(gs[9,:])
-            sl = Slider(axSlider, 'gamma', 0, 1e-12, valinit=0, valstep=1e-15)
+
+            if INPUT_IMG == 'generated': gamma_mag = -12 
+            elif INPUT_IMG =='aerial': gamma_mag = -16
+
+            sl = Slider(axSlider, 'gamma', 0, 10**(gamma_mag), valinit=0, valstep=10**((gamma_mag-3)))
 
             def sl_changed(sliderVal):
                 f_hat, phi = wienerCFiltering(g, h, sliderVal)
-                im.set_data(f_hat)
-                t.set_text("phi = {}".format(phi))
+                im.set_data(f_hat[0:M, 0:N])
+                t.set_text(r"$\phi(\gamma)$ = {:e}".format(phi))
 
             sl.on_changed(sl_changed)
 
             # Do filtering and show result
-            f_hat, _ = wienerCFiltering(g, h, 0)
-            im = axImg.imshow(np.clip(f_hat, 0, 1), cmap='gray')
+            f_hat, _ = wienerCFiltering(g, h, gamma=0) # initial plot: regular inverse filtering
+            im = axImg.imshow(np.clip(f_hat, 0, 1)[0:M, 0:N], cmap='gray')
 
     plt.show(block=True)
 
